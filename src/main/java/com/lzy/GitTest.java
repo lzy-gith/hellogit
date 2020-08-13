fb9faa0a7cf4d7587ff5be6039000b8d4814dda7
lzy-gith <63952987+lzy-gith@users.noreply.github.com>
2020-08-13 22:16:01 +0800

    Update GitTest.java

--- src/main/java/com/lzy/GitTest.java
+++ src/main/java/com/lzy/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("主干添加");
         System.out.println("分支添加");
         System.out.println("在线更新");
+        System.out.println("又更新了一次");
     }
 }
